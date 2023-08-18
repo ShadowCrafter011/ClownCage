98797b1c25293ba5f67808fbe8c720403022051f
ShadowCrafter011 <lkoe@bluewin.ch>
2023-08-19 01:12:12 +0200

    Ping server immediately after start

--- background_ping.js
+++ background_ping.js
@@ -18,47 +18,53 @@ socket.onopen = () => {
 };
 
 function ping() {
+    if (socket) ping_core();
+
     const pingIntervalId = setInterval(async () => {
         if (!socket) {
             clearInterval(pingIntervalId);
             return;
         }
 
-        uuid_container = await chrome.storage.sync.get(["uuid"])
+        ping_core();
+    }, 3000);
+}
 
-        let uuid = uuid_container.uuid;
-        if (!uuid) return;
-        
-        let tabs = await chrome.tabs.query({});
-        let [active_tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-        let hidden_status = { hidden: true }
+async function ping_core() {
+    uuid_container = await chrome.storage.sync.get(["uuid"]);
 
+    let uuid = uuid_container.uuid;
+    if (!uuid) return;
+    
+    let tabs = await chrome.tabs.query({});
+    let [active_tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    let hidden_status = { hidden: true }
+
+    try {
+        hidden_status = await chrome.tabs.sendMessage(active_tab.id, { query_hidden_status: true });
+    } catch (error) {}
+
+    let num_tabs = 0;
+    for (let tab of tabs) {
         try {
-            hidden_status = await chrome.tabs.sendMessage(active_tab.id, { query_hidden_status: true });
+            let extension_status = await chrome.tabs.sendMessage(tab.id, { query_extension: true });
+            if (extension_status.extension_active) {
+                num_tabs++;
+            }
         } catch (error) {}
+    }
 
-        let num_tabs = 0;
-        for (let tab of tabs) {
-            try {
-                let extension_status = await chrome.tabs.sendMessage(tab.id, { query_extension: true });
-                if (extension_status.extension_active) {
-                    num_tabs++;
-                }
-            } catch (error) {}
-        }
-
-        const ping_request = {
-            command: "message",
-            identifier: JSON.stringify({
-                channel: "ConsumerChannel"
-            }),
-            data: JSON.stringify({
-                action: "ping",
-                uuid: uuid,
-                num_tabs: num_tabs,
-                has_active: !hidden_status.hidden
-            })
-        }
-        socket.send(JSON.stringify(ping_request));
-    }, 5000);
+    const ping_request = {
+        command: "message",
+        identifier: JSON.stringify({
+            channel: "ConsumerChannel"
+        }),
+        data: JSON.stringify({
+            action: "ping",
+            uuid: uuid,
+            num_tabs: num_tabs,
+            has_active: !hidden_status.hidden
+        })
+    }
+    socket.send(JSON.stringify(ping_request));
 }
