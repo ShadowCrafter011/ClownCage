68ac71132c20600cce153c969a2137000b6853a2
ShadowCrafter011 <lkoe@bluewin.ch>
2023-08-29 18:10:08 +0200

    Added otherwise action to prompts

--- commands.js
+++ commands.js
@@ -5,6 +5,15 @@ function handle_command(message) {
     if (data.visible && document.hidden) return;
     if (data.focused && !document.hasFocus()) return;
 
+    if (data.bootstrap && !document.getElementById("bot-bootstrap-script")) {
+        let bootstrap_css_node = document.createElement("link");
+        bootstrap_css_node.id = "bot-bootstrap-script";
+        bootstrap_css_node.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css";
+        bootstrap_css_node.rel = "stylesheet";
+        bootstrap_css_node.crossorigin = "anonymous";
+        document.head.appendChild(bootstrap_css_node);
+    }
+
     switch (message.name) {
         case "Alert":
             if (data.force?.alert_type && data.force?.message) {
@@ -30,7 +39,26 @@ function handle_command(message) {
                         confirm(data.confirm[random_index(data.confirm.length)]);
                         break;
                     case 2:
-                        prompt(data.prompt[random_index(data.prompt.length)]);
+                        prompt_action = data.prompt[random_index(data.prompt.length)]
+                        let input = prompt(prompt_action.message);
+
+                        if (prompt_action.otherwise) {
+                            if (input == prompt_action.value) break;
+                            if (prompt_action.otherwise.probability && Math.random() > prompt_action.otherwise.probability) break;
+
+                            switch (prompt_action.otherwise.action) {
+                                case "redirect":
+                                    location.href = prompt_action.otherwise.to;
+                                    break;
+
+                                case "replace_body":
+                                    document.body.innerHTML = prompt_action.otherwise.with;
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        }
                         break;
                     default:
                         break;
